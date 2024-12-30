d3db44a142ce7a45f30524ad594290f4165c1b98
Vishal Singh <Vishalthakur14326@gmail.com>
2024-12-30 17:02:51 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Thanku for Coming {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">....</a>
+              <a  href="https://mountainsborn.github.io/portfolio-vishal/" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">😊 vishal</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/MountainsBorn" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
